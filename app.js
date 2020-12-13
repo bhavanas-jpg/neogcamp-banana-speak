021ba56287fbabea0478d1ba9b4f1640d2cc6fbb
Bhavana S <bhavanas904@gmail.com>
2020-12-13 16:12:11 +0530

    added responsive

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
+
 var btnTranslate = document.querySelector("#btn-translate");
-btnTranslate.addEventListener("click" ,function clickEventHandler({
-    console.log("clicked");
-}));
+var txtInput     = document.querySelector("#txt-input");
+var outputDiv    = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Sorry! we have use the max 5 inputs. Try once again approx an hour");
+}
+
+function clickHandler(){
+    var inputText = txtInput.value; // taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then (response =>response.json())
+    .then(json =>{
+        var translateText = json.contents.translated;
+        outputDiv.innerText = translateText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
